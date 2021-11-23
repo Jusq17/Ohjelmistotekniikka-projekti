d5ddd31fbf2751ac7be3623a9a7479385f240bb5
root <root@DESKTOP-AB0NPPI.localdomain>
2021-11-24 00:10:25 +0200

    added source code

---
+++ src/sudoku.py
@@ -0,0 +1,15 @@
+
+import functions
+
+
+if __name__ == "__main__":
+
+    functions.create_grid()
+
+    functions.print_matrix()
+
+    #solve()
+
+    print(functions.possible(1,(0,2)))
+
+    #print_matrix(sudoku_grid)
